f79b6da99c557e254bcf8c7c8be55a20020c8258
elisadg <e.degregorio@ocado.com>
2019-10-31 14:50:42 +0100

    initial files and structures

--- app/src/main/java/com/example/androidstarter/di/NetworkModule.kt
+++ app/src/main/java/com/example/androidstarter/di/NetworkModule.kt
@@ -1,9 +1,48 @@
 package com.example.androidstarter.di
 
+import com.example.androidstarter.BuildConfig
+import com.example.androidstarter.features.list.data.PlantsApi
 import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 class NetworkModule {
 
+    @Provides
+    @Singleton
+    internal fun provideNetworkClient(): Retrofit{
+        val baseUrl ="www"
+        val loggingInterceptor = HttpLoggingInterceptor()
+
+        if (BuildConfig.DEBUG) {
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        } else {
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BASIC
+        }
+
+        val okHttpClient = OkHttpClient().newBuilder()
+            .readTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(baseUrl)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    internal fun providePlantsApi(apiClient: Retrofit): PlantsApi {
+        return apiClient.create(PlantsApi::class.java)
+    }
 
 }
--- app/src/main/java/com/example/androidstarter/features/list/domain/PlantRepository.kt
+++ app/src/main/java/com/example/androidstarter/features/list/domain/PlantRepository.kt
@@ -1,11 +1,10 @@
 package com.example.androidstarter.features.list.domain
 
 import com.example.androidstarter.features.list.data.PlantsApi
+import javax.inject.Inject
 
-class PlantProvider (private val plantApi: PlantsApi, private val plantsMapper: PlantsMapper){
-
-
-    suspend fun getPlants():List<Plant>{
+class PlantRepository  @Inject constructor(private val plantApi: PlantsApi, private val plantsMapper: PlantsMapper) {
+    suspend fun getPlants(): List<Plant> {
         return plantApi.getAllPlants("plantica").map { plantsMapper.mapToModel(it) }
     }
 }
