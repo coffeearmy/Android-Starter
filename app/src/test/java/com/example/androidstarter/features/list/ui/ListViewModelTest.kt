001074176bf66e39da43d2d21ec731ad03ac9234
elisadg <e.degregorio@ocado.com>
2021-02-22 08:46:16 +0100

    Added Unit tests

---
+++ app/src/test/java/com/example/androidstarter/features/list/ui/ListViewModelTest.kt
@@ -0,0 +1,5 @@
+package com.example.androidstarter.features.list.ui
+
+import org.junit.Assert.*
+
+class ListViewModelTest
---
+++ app/src/test/java/com/example/androidstarter/utils/LiveDataTestUtil.kt
@@ -0,0 +1,4 @@
+package com.example.androidstarter.utils
+
+class LiveDataTestUtil {
+}
